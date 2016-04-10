d666bea7f98ccb6367cf88405956343bc7ff93dd
Hyeonkee-Jo <201202143@cs-cnu.org>
2016-04-10 23:48:09 +0900

    d

--- src/main/java/bill/App.java
+++ src/main/java/bill/App.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 
 public class App {
 	
-	private App(){
-		
-	}
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger("Bill");
 		@SuppressWarnings("resource")
