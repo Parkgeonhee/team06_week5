4bd503236360bd59fb78db9cf7174d61fb46663f
Hyeonkee-Jo <201202143@cs-cnu.org>
2016-04-10 23:44:00 +0900

    a

--- src/test/java/bill/AppTest.java
+++ src/test/java/bill/AppTest.java
@@ -16,110 +16,100 @@ public class AppTest{
 		c = new Cost(null, 0, 0);
 	}
 	
-	public void set(String p, int traffic , int num){
-		c.traffic = traffic;
-		c.num = num;
-		if(("S").equals(p)){
-			c.plan = new SilverPlan();		
-		}
-		else if(("G").equals(p)) {
-			c.plan = new GoldPlan();
-		}
-	}
 	
 	@Test
 	public void testGoldOnelineBelowThau() {
-		set("G", 700, 1);
+		c = new Cost("G", 700, 1);
 		assertEquals("49.95$", c.toString());
 	}
 	
 	@Test
 	public void testGoldTwolineBelowThau() {
-		set("G", 700, 2);
+		c = new Cost("G", 700, 2);
 		assertEquals("64.45$", c.toString());
 	}
 	
 	@Test
 	public void testGoldThreelineBelowThau() {
-		set("G", 700, 3);
+		c = new Cost("G", 700, 3);
 		assertEquals("78.95$", c.toString());
 	}
 	
 	@Test
 	public void testGoldOverThreelineBelowThau() {
-		set("G", 700, 4);
+		c = new Cost("G", 700, 4);
 		assertEquals("83.95$", c.toString());
 	}
 	
 	@Test
 	public void testGoldOnelineOverThau() {
-		set("G", 1200, 1);
+		c = new Cost("G", 1200, 1);
 		assertEquals("139.95$", c.toString());
 	}
 	
 	@Test
 	public void testGoldTwolineOverThau() {
-		set("G", 1200, 2);
+		c = new Cost("G", 1200, 2);
 		assertEquals("154.45$", c.toString());
 	}
 	
 	@Test
 	public void testGoldThreelineOverThau() {
-		set("G", 1200, 3);
+		c = new Cost("G", 1200, 3);
 		assertEquals("168.95$", c.toString());
 	}
 	
 	@Test
 	public void testGoldOverThreelineOverThau() {
-		set("G", 1200, 4);
+		c = new Cost("G", 1200, 4);
 		assertEquals("173.95$", c.toString());
 	}
 	
 	@Test
 	public void testSilverOnelineBelowFiveHund() {
-		set("S", 400, 1);
+		c = new Cost("S", 400, 1);
 		assertEquals("29.95$", c.toString());
 	}
 	
 	@Test
 	public void testSilverTwolineBelowFiveHund() {
-		set("S", 400, 2);
+		c = new Cost("S", 400, 2);
 		assertEquals("51.45$", c.toString());
 	}
 	
 	@Test
 	public void testSilverThreelineBelowFiveHund() {
-		set("S", 400, 3);
+		c = new Cost("S", 400, 3);
 		assertEquals("72.95$", c.toString());
 	}
 	
 	@Test
 	public void testSilverOverThreelineBelowFiveHund() {
-		set("S", 400, 4);
+		c = new Cost("S", 400, 4);
 		assertEquals("77.95$", c.toString());
 	}
 	
 	@Test
 	public void testSilverOnelineOverFiveHund() {
-		set("S", 700, 1);
+		c = new Cost("S", 700, 1);
 		assertEquals("137.95$", c.toString());
 	}
 	
 	@Test
 	public void testSilverTwolineOverFiveHund() {
-		set("S", 700, 2);
+		c = new Cost("S", 700, 2);
 		assertEquals("159.45$", c.toString());
 	}
 	
 	@Test
 	public void testSilverThreelineOverFiveHund() {
-		set("S", 700, 3);
+		c = new Cost("S", 700, 3);
 		assertEquals("180.95$", c.toString());
 	}
 	
 	@Test
 	public void testSilverOverThreelineOverFiveHund() {
-		set("S", 700, 4);
+		c= new Cost("S", 700, 4);
 		assertEquals("185.95$", c.toString());
 	}
 }
